21e8c2ebb34d028bd8882e69c3a9652014dfa755
곽태욱 <gwak2837@kakao.com>
2021-01-05 23:30:55 +0900

    Create sample pages

---
+++ src/pages/my/index.tsx
@@ -0,0 +1,18 @@
+import PageLayout from 'src/components/layouts/PageLayout'
+import PageTitle from 'src/components/layouts/PageTitle'
+import LoginForm from 'src/components/LoginForm'
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {}
+
+// eslint-disable-next-line no-empty-pattern
+function MyPage({}: Props) {
+  const login = false
+  return (
+    <PageTitle title="Icecream Music - My Page">
+      <PageLayout>{login ? 'My Page' : <LoginForm />}</PageLayout>
+    </PageTitle>
+  )
+}
+
+export default MyPage
