6561d3bc34999a0d026ddbec2bf2582162800418
곽태욱 <gwak2837@kakao.com>
2021-01-12 00:00:02 +0900

    WIP: Add icezam logo
    로고 너비 높이가 자동 조절되게 설정해야 함

--- src/components/atoms/IcezamLogo.tsx
+++ src/components/atoms/IcezamLogo.tsx
@@ -1,17 +1,18 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {}
 
 // eslint-disable-next-line no-empty-pattern
-function IcecreamLogo({}: Props) {
+function IcezamLogo({}: Props) {
   return (
     <Link href="/">
       <a href="/">
-        <img src="/icecream-logo.png" alt="icecream-logo" />
+        <Image src="/icezam-logo.png" alt="icezam-logo" width={50} height={50} />
       </a>
     </Link>
   )
 }
 
-export default IcecreamLogo
+export default IcezamLogo
