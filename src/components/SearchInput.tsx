03047bf5c60f00b527c3af9c287a83b07442d8f7
곽태욱 <gwak2837@kakao.com>
2021-01-13 23:27:05 +0900

    Rename and move a SearchForm component

--- src/components/SearchInput.tsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Props = {}
-
-// eslint-disable-next-line no-empty-pattern
-function SearchInput({}: Props) {
-  return <input placeholder="검색어를 입력해주세요"></input>
-}
-
-export default SearchInput
