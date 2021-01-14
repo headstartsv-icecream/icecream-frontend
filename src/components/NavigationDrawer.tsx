bdef791545fe117258385b27f7261f8c1e07b45f
곽태욱 <gwak2837@kakao.com>
2021-01-14 20:03:14 +0900

    Modify css style and layout

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -1,3 +1,4 @@
+import { CloseOutlined } from '@ant-design/icons'
 import Link from 'next/link'
 import styled from 'styled-components'
 import ClientPortal from './atoms/ClientPortal'
@@ -10,11 +11,21 @@ const AbsolutePosition = styled.div<{ doesDrawerOpen: boolean }>`
   inset: 0;
   z-index: 1;
 
-  background-color: #888;
   overflow-y: auto;
+  background-color: #111;
 
   ${(p) => (p.doesDrawerOpen ? 'transform: translate(0, 0);' : 'transform: translate(101%, 0);')}
   transition: transform 0.3s cubic-bezier(0.4, 0.2, 0, 1);
+
+  a,
+  div {
+    color: white;
+    font-size: 1.2rem;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
 `
 
 const FlexContainerBetween = styled.div`
@@ -25,9 +36,17 @@ const FlexContainerBetween = styled.div`
   align-items: center;
 `
 
-const FlexContainerColumn = styled.div`
+const StyledCloseOutlined = styled(CloseOutlined)`
+  font-size: 1.5rem;
+`
+
+const FlexContainerColumn = styled.ul`
+  margin: 3rem 0;
   display: flex;
   flex-flow: column nowrap;
+  gap: 2rem;
+
+  list-style-type: none;
 `
 
 type Props = {
@@ -43,17 +62,19 @@ function NavigationDrawer({ closeDrawer, doesDrawerOpen }: Props) {
           <div onClick={closeDrawer} onKeyDown={closeDrawer} role="button" tabIndex={0}>
             <IcezamLogo />
           </div>
-          <div onClick={closeDrawer} onKeyDown={closeDrawer} role="button" tabIndex={0}>
-            X
-          </div>
+          <StyledCloseOutlined onClick={closeDrawer} />
         </FlexContainerBetween>
         <FlexContainerColumn>
-          <Link href="myicezam">
-            <a href="myicezam">내 라이브러리</a>
-          </Link>
-          <Link href="charts">
-            <a href="charts">차트</a>
-          </Link>
+          <li>
+            <Link href="myicezam">
+              <a href="myicezam">내 라이브러리</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="charts">
+              <a href="charts">차트</a>
+            </Link>
+          </li>
         </FlexContainerColumn>
       </AbsolutePosition>
     </ClientPortal>
