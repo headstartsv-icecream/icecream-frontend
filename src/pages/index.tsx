b236e671444fc72f61cde141beb9b730f4024a9b
곽태욱 <gwak2837@kakao.com>
2021-01-15 12:14:24 +0900

    Modify structure of components
    pages/index.tsx : 글이랑 녹음 버튼 렌더링
    RecorderModal.tsx : 마이크를 녹음하고 API를 호출하는 모달

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,72 @@
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import DragDrop from 'src/components/atoms/DragDrop'
 import PageLayout from 'src/components/layouts/PageLayout'
 import PageTitle from 'src/components/layouts/PageTitle'
-import Recorder from 'src/components/Recorder'
-import styled from 'styled-components'
+import RecorderModal from 'src/components/RecorderModal'
+import styled, { keyframes } from 'styled-components'
+
+const FlexContainerColumn = styled.div`
+  min-height: 100vh;
+  padding-top: 4rem;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  background: linear-gradient(to bottom, #0bf, #066aff);
+
+  h1,
+  h2 {
+    color: #fff;
+    font-weight: bold;
+    text-align: center;
+    word-break: keep-all;
+  }
+`
+
+const H1 = styled.h1`
+  font-size: 3rem;
+  margin: 1rem;
+`
+
+const H2 = styled.h2`
+  margin: 1rem;
+`
+
+const MaxWidth = styled.div`
+  max-width: 350px;
+`
+
+const breathingButton = keyframes`
+  0%, 100% {
+    transform: scale(1,1);
+  }
+  50% {
+    transform: scale(1.1,1.1);
+  }
+`
+
+const AnimatedImage = styled(Image)`
+  filter: drop-shadow(0 0 20px rgba(0, 0, 0, 0.3));
+  stroke: rgba(255, 255, 255, 0.1);
+  stroke-width: 2px;
+  animation: ${breathingButton} 3s infinite ease-in-out;
+
+  :hover {
+    cursor: pointer;
+  }
+`
 
 const FlexContainer = styled.div`
   display: flex;
   flex-flow: row nowrap;
 `
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const gif = require('../../public/equalizer.gif')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const logo = require('../../public/icezam-logo.png')
 
 function HomePage() {
   const [musicInfo, setMusicInfo] = useState({})
+  const [isRecorderModalOpen, setIsRecorderModalOpen] = useState(false)
+
   useEffect(() => {
     console.log(musicInfo)
   }, [musicInfo])
@@ -23,7 +74,30 @@ function HomePage() {
   return (
     <PageTitle title="Icezam - 음악을 검색하고, 음악에 대한 다양한 사람들의 반응을 알아보는 공간">
       <PageLayout>
-        <Recorder setMusicInfo={setMusicInfo} />
+        <FlexContainerColumn>
+          <H1>Icezam은 주변에서 들리는 곡을 인식합니다.</H1>
+          <H2>클릭하여 Icezam하기</H2>
+          <MaxWidth>
+            <AnimatedImage
+              src="/icezam-logo.png"
+              alt="icezam-logo"
+              width={500}
+              height={500}
+              onClick={() => {
+                setIsRecorderModalOpen(true)
+              }}
+            />
+          </MaxWidth>
+        </FlexContainerColumn>
+
+        {isRecorderModalOpen && (
+          <RecorderModal
+            isOpen={isRecorderModalOpen}
+            setIsOpen={setIsRecorderModalOpen}
+            setMusicInfo={setMusicInfo}
+          />
+        )}
+
         <DragDrop setMusicInfo={setMusicInfo} />
 
         <FlexContainer>
