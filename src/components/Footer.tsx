52bdbe1c7f7c47839c103097a39a5a938fb96027
곽태욱 <gwak2837@kakao.com>
2021-01-17 00:59:27 +0900

    Remove props

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Props = {}
-
-// eslint-disable-next-line no-empty-pattern
-function Footer({}: Props) {
+function Footer() {
   return <footer>footer</footer>
 }
 
