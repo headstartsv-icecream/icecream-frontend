905edf18ec295abb86f8ddb05061a3c31b0a0187
곽태욱 <gwak2837@kakao.com>
2021-01-21 23:23:10 +0900

    Add skeleton of artist detail page

---
+++ src/pages/artists/[id]/index.tsx
@@ -0,0 +1,12 @@
+import PageLayout from 'src/components/layouts/PageLayout'
+import PageTitle from 'src/components/layouts/PageTitle'
+
+function ArtistDetailPage() {
+  return (
+    <PageTitle title="Icezam - Artist Detail Page">
+      <PageLayout>Artist Detail Page</PageLayout>
+    </PageTitle>
+  )
+}
+
+export default ArtistDetailPage
