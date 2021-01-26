7e907e0960ac738c6a24a3e08cb19368eb310eca
곽태욱 <gwak2837@kakao.com>
2021-01-26 23:12:18 +0900

    Delete sample graphql files and Create music graphql

--- src/graphql/generated/types-and-hooks.ts
+++ src/graphql/generated/types-and-hooks.ts
@@ -16,22 +16,109 @@ export type Scalars = {
   DateTime: any
 }
 
-export type CCreationInput = {
-  name: Scalars['String']
-  age: Scalars['Int']
+/** 댓글 생성 시 필요한 입력값 */
+export type CommentCreationInput = {
+  content: Scalars['String']
+  userName: Scalars['String']
+}
+
+/** 댓글 수정 시 필요한 입력값 */
+export type CommentModificationInput = {
+  id: Scalars['ID']
+  content: Scalars['String']
 }
 
 export type Mutation = {
   __typename?: 'Mutation'
-  createC: C
+  createComment: Scalars['Boolean']
+  modifyComment: Scalars['Boolean']
+  deleteComment: Scalars['Boolean']
+  /** shazamId가 있으면 기존 레코드를 수정하고, shazamId가 없으면 새로 만든다. */
+  createOrModifyMusic: Music
 }
 
-export type MutationCreateCArgs = {
-  input: CCreationInput
+export type MutationCreateCommentArgs = {
+  input: CommentCreationInput
 }
 
-export type C = {
-  __typename?: 'C'
+export type MutationModifyCommentArgs = {
+  input: CommentModificationInput
+}
+
+export type MutationDeleteCommentArgs = {
+  id: Scalars['ID']
+}
+
+export type MutationCreateOrModifyMusicArgs = {
+  input: MusicCreationModificationInput
+}
+
+/** 음악 정보 생성-수정 시 필요한 입력값 */
+export type MusicCreationModificationInput = {
+  shazamId: Scalars['ID']
+  title?: Maybe<Scalars['String']>
+  artists?: Maybe<Array<Scalars['String']>>
+  genres?: Maybe<Array<Scalars['String']>>
+  lyrics?: Maybe<Array<Scalars['String']>>
+  comments?: Maybe<Array<Scalars['String']>>
+  youtubeLink?: Maybe<Scalars['String']>
+  youtubeImage?: Maybe<Scalars['String']>
+  artistImage?: Maybe<Scalars['String']>
+  albumImage?: Maybe<Scalars['String']>
+  similarMusics?: Maybe<Array<Scalars['ID']>>
+  artistOtherMusics?: Maybe<Array<Scalars['ID']>>
+  includedPlaylists?: Maybe<Array<Scalars['ID']>>
+}
+
+export enum CrawlingSource {
+  Youtube = 'YOUTUBE',
+  Melon = 'MELON',
+}
+
+export type Comment = {
+  __typename?: 'Comment'
+  id: Scalars['ID']
+  creationDate: Scalars['DateTime']
+  crawlingDate: Scalars['DateTime']
+  content: Scalars['String']
+  userName: Scalars['String']
+  source: CrawlingSource
+  like?: Maybe<Scalars['Int']>
+}
+
+export type Music = {
+  __typename?: 'Music'
+  id: Scalars['ID']
+  title: Scalars['String']
+  artists: Array<Scalars['String']>
+  searchCount: Scalars['Int']
+  albumImage?: Maybe<Scalars['String']>
+  artistImage?: Maybe<Scalars['String']>
+  genres?: Maybe<Array<Scalars['String']>>
+  lyrics?: Maybe<Array<Scalars['String']>>
+  melonLink?: Maybe<Scalars['String']>
+  shazamId?: Maybe<Scalars['Int']>
+  youtubeLink?: Maybe<Scalars['String']>
+  youtubeImage?: Maybe<Scalars['String']>
+  /** 이 노래를 부른 가수의 다른 노래를 검색 횟수 순으로 반환한다. # 페이지네이션 필요 */
+  artistOtherMusics?: Maybe<Array<Music>>
+  /** 이 노래에 해당하는 댓글 목록을 반환한다. # 페이지네이션 필요 */
+  comments?: Maybe<Array<Scalars['String']>>
+  /** 이 노래가 포함된 재생 목록을 반환한다. # 페이지네이션 필요 */
+  includedPlaylists?: Maybe<Array<Playlist>>
+  /** 이 노래와 비슷한 노래 목록을 반환한다. # 페이지네이션 필요 */
+  similarMusics?: Maybe<Array<Music>>
+}
+
+export type Playlist = {
+  __typename?: 'Playlist'
+  id: Scalars['ID']
+  name: Scalars['String']
+  musics?: Maybe<Array<Music>>
+}
+
+export type User = {
+  __typename?: 'User'
   id: Scalars['ID']
   creationDate: Scalars['DateTime']
   name: Scalars['String']
@@ -40,58 +127,196 @@ export type C = {
 
 export type Query = {
   __typename?: 'Query'
-  /** 특정 C 정보를 반환한다. */
-  cById?: Maybe<C>
+  comments?: Maybe<Array<Comment>>
+  comment?: Maybe<Comment>
+  /** 모든 음악 목록을 반환한다. # 페이지네이션 필요 */
+  musics?: Maybe<Array<Music>>
+  /** 특정 음악 정보를 반환한다. */
+  music?: Maybe<Music>
+  /** 노래 제목 및 가수 이름으로 음악 검색 */
+  musicByTitleArtist?: Maybe<Music>
+  /** 사용자 목록을 반환한다. (관리자 전용) */
+  users?: Maybe<Array<User>>
+  /** 내 정보를 반환한다. 해당 권한이 없으면 오류가 발생한다. */
+  me?: Maybe<User>
 }
 
-export type QueryCByIdArgs = {
-  cId: Scalars['Int']
+export type QueryCommentArgs = {
+  id: Scalars['ID']
 }
 
-export type CByIdQueryVariables = Exact<{ [key: string]: never }>
-
-export type CByIdQuery = { __typename?: 'Query' } & {
-  cById?: Maybe<{ __typename?: 'C' } & Pick<C, 'id' | 'name' | 'age'>>
+export type QueryMusicArgs = {
+  id: Scalars['ID']
 }
 
-export const CByIdDocument = gql`
-  query CById {
-    cById(cId: 1) {
+export type QueryMusicByTitleArtistArgs = {
+  title: Scalars['String']
+  artist?: Maybe<Array<Scalars['String']>>
+}
+
+export type CreateOrModifyMusicMutationVariables = Exact<{
+  input: MusicCreationModificationInput
+}>
+
+export type CreateOrModifyMusicMutation = { __typename?: 'Mutation' } & {
+  createOrModifyMusic: { __typename?: 'Music' } & Pick<Music, 'id' | 'title'>
+}
+
+export type MusicQueryVariables = Exact<{
+  id: Scalars['ID']
+}>
+
+export type MusicQuery = { __typename?: 'Query' } & {
+  music?: Maybe<{ __typename?: 'Music' } & Pick<Music, 'id'>>
+}
+
+export type MusicByTitleArtistQueryVariables = Exact<{
+  title: Scalars['String']
+  artist?: Maybe<Array<Scalars['String']> | Scalars['String']>
+}>
+
+export type MusicByTitleArtistQuery = { __typename?: 'Query' } & {
+  musicByTitleArtist?: Maybe<{ __typename?: 'Music' } & Pick<Music, 'id'>>
+}
+
+export const CreateOrModifyMusicDocument = gql`
+  mutation CreateOrModifyMusic($input: MusicCreationModificationInput!) {
+    createOrModifyMusic(input: $input) {
       id
-      name
-      age
+      title
+    }
+  }
+`
+export type CreateOrModifyMusicMutationFn = Apollo.MutationFunction<
+  CreateOrModifyMusicMutation,
+  CreateOrModifyMusicMutationVariables
+>
+
+/**
+ * __useCreateOrModifyMusicMutation__
+ *
+ * To run a mutation, you first call `useCreateOrModifyMusicMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateOrModifyMusicMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createOrModifyMusicMutation, { data, loading, error }] = useCreateOrModifyMusicMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useCreateOrModifyMusicMutation(
+  baseOptions?: Apollo.MutationHookOptions<
+    CreateOrModifyMusicMutation,
+    CreateOrModifyMusicMutationVariables
+  >
+) {
+  return Apollo.useMutation<CreateOrModifyMusicMutation, CreateOrModifyMusicMutationVariables>(
+    CreateOrModifyMusicDocument,
+    baseOptions
+  )
+}
+export type CreateOrModifyMusicMutationHookResult = ReturnType<
+  typeof useCreateOrModifyMusicMutation
+>
+export type CreateOrModifyMusicMutationResult = Apollo.MutationResult<CreateOrModifyMusicMutation>
+export type CreateOrModifyMusicMutationOptions = Apollo.BaseMutationOptions<
+  CreateOrModifyMusicMutation,
+  CreateOrModifyMusicMutationVariables
+>
+export const MusicDocument = gql`
+  query Music($id: ID!) {
+    music(id: $id) {
+      id
     }
   }
 `
 
 /**
- * __useCByIdQuery__
+ * __useMusicQuery__
  *
- * To run a query within a React component, call `useCByIdQuery` and pass it any options that fit your needs.
- * When your component renders, `useCByIdQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * To run a query within a React component, call `useMusicQuery` and pass it any options that fit your needs.
+ * When your component renders, `useMusicQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
  *
  * @example
- * const { data, loading, error } = useCByIdQuery({
+ * const { data, loading, error } = useMusicQuery({
  *   variables: {
+ *      id: // value for 'id'
  *   },
  * });
  */
-export function useCByIdQuery(
-  baseOptions?: Apollo.QueryHookOptions<CByIdQuery, CByIdQueryVariables>
+export function useMusicQuery(
+  baseOptions: Apollo.QueryHookOptions<MusicQuery, MusicQueryVariables>
 ) {
-  return Apollo.useQuery<CByIdQuery, CByIdQueryVariables>(CByIdDocument, baseOptions)
+  return Apollo.useQuery<MusicQuery, MusicQueryVariables>(MusicDocument, baseOptions)
 }
-export function useCByIdLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<CByIdQuery, CByIdQueryVariables>
+export function useMusicLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<MusicQuery, MusicQueryVariables>
 ) {
-  return Apollo.useLazyQuery<CByIdQuery, CByIdQueryVariables>(CByIdDocument, baseOptions)
+  return Apollo.useLazyQuery<MusicQuery, MusicQueryVariables>(MusicDocument, baseOptions)
 }
-export type CByIdQueryHookResult = ReturnType<typeof useCByIdQuery>
-export type CByIdLazyQueryHookResult = ReturnType<typeof useCByIdLazyQuery>
-export type CByIdQueryResult = Apollo.QueryResult<CByIdQuery, CByIdQueryVariables>
+export type MusicQueryHookResult = ReturnType<typeof useMusicQuery>
+export type MusicLazyQueryHookResult = ReturnType<typeof useMusicLazyQuery>
+export type MusicQueryResult = Apollo.QueryResult<MusicQuery, MusicQueryVariables>
+export const MusicByTitleArtistDocument = gql`
+  query MusicByTitleArtist($title: String!, $artist: [String!]) {
+    musicByTitleArtist(title: $title, artist: $artist) {
+      id
+    }
+  }
+`
+
+/**
+ * __useMusicByTitleArtistQuery__
+ *
+ * To run a query within a React component, call `useMusicByTitleArtistQuery` and pass it any options that fit your needs.
+ * When your component renders, `useMusicByTitleArtistQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useMusicByTitleArtistQuery({
+ *   variables: {
+ *      title: // value for 'title'
+ *      artist: // value for 'artist'
+ *   },
+ * });
+ */
+export function useMusicByTitleArtistQuery(
+  baseOptions: Apollo.QueryHookOptions<MusicByTitleArtistQuery, MusicByTitleArtistQueryVariables>
+) {
+  return Apollo.useQuery<MusicByTitleArtistQuery, MusicByTitleArtistQueryVariables>(
+    MusicByTitleArtistDocument,
+    baseOptions
+  )
+}
+export function useMusicByTitleArtistLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    MusicByTitleArtistQuery,
+    MusicByTitleArtistQueryVariables
+  >
+) {
+  return Apollo.useLazyQuery<MusicByTitleArtistQuery, MusicByTitleArtistQueryVariables>(
+    MusicByTitleArtistDocument,
+    baseOptions
+  )
+}
+export type MusicByTitleArtistQueryHookResult = ReturnType<typeof useMusicByTitleArtistQuery>
+export type MusicByTitleArtistLazyQueryHookResult = ReturnType<
+  typeof useMusicByTitleArtistLazyQuery
+>
+export type MusicByTitleArtistQueryResult = Apollo.QueryResult<
+  MusicByTitleArtistQuery,
+  MusicByTitleArtistQueryVariables
+>
 
 export interface PossibleTypesResultData {
   possibleTypes: {
