6bb53d240b9597f0c0cf2ec514239d1ad69d1401
곽태욱 <gwak2837@kakao.com>
2021-01-26 23:13:29 +0900

    Minor changes

--- src/pages/artists/[id]/index.tsx
+++ src/pages/artists/[id]/index.tsx
@@ -1,10 +1,26 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import PageLayout from 'src/components/layouts/PageLayout'
 import PageTitle from 'src/components/layouts/PageTitle'
+import { HEADER_HEIGHT } from 'src/models/constants'
+import styled from 'styled-components'
+
+const Padding = styled.div`
+  padding: ${HEADER_HEIGHT} 0 0 0;
+`
 
 function ArtistDetailPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    console.log(router.query)
+  }, [router])
+
   return (
     <PageTitle title="Icezam - Artist Detail Page">
-      <PageLayout>Artist Detail Page</PageLayout>
+      <PageLayout>
+        <Padding>Artist Detail Page</Padding>
+      </PageLayout>
     </PageTitle>
   )
 }
