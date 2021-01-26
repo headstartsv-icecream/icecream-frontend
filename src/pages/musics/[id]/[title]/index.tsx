376333c84fd939cfb5da776bf9e97b97d7c3cb0f
곽태욱 <gwak2837@kakao.com>
2021-01-26 23:13:46 +0900

    Rename file

--- src/pages/musics/[id]/[title]/index.tsx
+++ src/pages/musics/[id]/[title]/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import PageLayout from 'src/components/layouts/PageLayout'
 import PageTitle from 'src/components/layouts/PageTitle'
 import MusicNameOverlay from 'src/components/MusicNameOverlay'
+import { useMusicQuery } from 'src/graphql/generated/types-and-hooks'
 import { HEADER_HEIGHT, imgUrl, MOBILE_MIN_WIDTH } from 'src/models/constants'
 import { getBlackOrWhiteTextColorFrom } from 'src/utils/commons'
 import styled from 'styled-components'
@@ -10,8 +11,9 @@ import styled from 'styled-components'
 const FlexContainer = styled.div<{ backgroundColor: string }>`
   width: 100%;
   padding: ${HEADER_HEIGHT} 0 0 0;
+  position: relative;
 
-  background-color: ${(p) => p.backgroundColor};
+  ${(p) => `background: linear-gradient(${p.backgroundColor} 50%, white 50%);`}
 
   display: flex;
   flex-flow: row wrap;
@@ -22,32 +24,35 @@ const FlexContainer = styled.div<{ backgroundColor: string }>`
 `
 
 const StyledImage = styled.img`
-  min-width: ${MOBILE_MIN_WIDTH};
+  width: 100%;
+  max-width: ${MOBILE_MIN_WIDTH};
+  height: auto;
+  object-fit: cover;
 `
 
 const MusicInformation = styled.div``
 
-export function MusicDetailPage() {
+function MusicDetailPage() {
   const router = useRouter()
-  const id = router.query.id
+  const { id, title } = router.query
 
-  // 서버로 음악 정보랑 댓글 정보 요청 보내고 받기
-  // const { data, error, loading } = use___Query({ variables: ... })
-  const musicName = 'Icezam-mite'
+  const { data, error, loading } = useMusicQuery({ variables: { id: id as string } })
 
   const backgroundColor = '#c8bebb'
 
   return (
     <PageTitle title="Icezam - My Page">
       <PageLayout>
-        <MusicNameOverlay backgroundColor={backgroundColor} musicName={musicName} />
+        <MusicNameOverlay backgroundColor={backgroundColor} musicName={title as string} />
         <FlexContainer backgroundColor={backgroundColor}>
           <StyledImage src={imgUrl} alt="music cover" />
+
           <MusicInformation>
             <div>맑은 하늘</div>
             <Link href="/artists/12">
               <a href="/artists/12">陳致逸 & HOYO-MiX</a>
             </Link>
+            ``
             <div>OST</div>
             <div>Icezam 268회</div>
             <div>음악 상세 페이지 ID: {id}</div>
